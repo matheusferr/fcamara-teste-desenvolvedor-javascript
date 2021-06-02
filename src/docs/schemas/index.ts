cb513e201d06c4a3dde0ddc1e525bc26796ddb3b
Matheus Ferreira <math.ferr@outlook.com>
2021-06-02 11:09:09 -0300

    Fixed docs body

--- src/docs/schemas/index.ts
+++ src/docs/schemas/index.ts
@@ -1,6 +1,6 @@
 import { pais } from "./pais";
-import { estado, estadoCreation } from "./estado";
-import { cidade, cidadeCreation } from "./cidade";
+import { estado, estadoCreation, estadoBody } from "./estado";
+import { cidade, cidadeCreation, cidadeBody } from "./cidade";
 import { pessoa, pessoaCreation, pessoaBody } from "./pessoa";
 import { error400, error404 } from "./error";
 
@@ -8,8 +8,10 @@ export default {
   pais,
   estado,
   estadoCreation,
+  estadoBody,
   cidade,
   cidadeCreation,
+  cidadeBody,
   pessoa,
   pessoaCreation,
   pessoaBody,
